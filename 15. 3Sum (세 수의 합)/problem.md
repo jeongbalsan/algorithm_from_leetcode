92f492b882c65a5734f4b66a2479c5343a00dcbb
chanmini <wjfeothtl@gmail.com>
2021-10-28 18:03:43 +0900

    Problem 15 is solved
    But need a reminder later

---
+++ 15. 3Sum (세 수의 합)/problem.md
@@ -0,0 +1,24 @@
+## 15. 3Sum
+## 15. 세 수의 합
+
+<br>
+
+### 문제 : 
+
+**Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0. Notice that the solution set must not contain duplicate triplets.**
+
+**배열을 입력받아 합으로 0을 만들 수 있는 3개의 엘리먼트를 출력하라.**
+
+--------------------------------------
+
+예제 1:
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+
+예제 2:
+Input: nums = []
+Output: []
+
+예제 3:
+Input: nums = [0]
+Output: []
---
+++ 15. 3Sum (세 수의 합)/source_code.py
@@ -0,0 +1,63 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        results = []
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            for j in range(i + 1, len(nums) - 1):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+
+                for k in range(j + 1, len(nums)):
+                    if k > j and nums[k] == nums[k - 1]:
+                        continue
+                    
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        results.append([nums[i], nums[j], nums[k]])
+
+        return results
+
+    def threeSum_1(self, nums: list[int]) -> list[list[int]]:
+        results = []
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left, right = i + 1, len(nums) - 1
+
+            while left < right:
+                sum = nums[i] + nums[left] + nums[right]
+
+                if sum < 0 :
+                    left += 1
+                elif sum > 0:
+                    right -= 1
+                else:
+                    results.append([nums[i], nums[left], nums[right]])
+
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+
+
+        return results
+
+
+
+if __name__ == "__main__":
+
+    nums = [-1,0,1,2,-1,-4]
+
+    cs = Solution()
+
+    print(cs.threeSum_1(nums))
+
+
